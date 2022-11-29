b99636aa549ee0646533c0a1a71ee728166c9ad4
pankajkumbhar <pankajkumbhar10@gmail.com>
2022-11-30 00:14:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
